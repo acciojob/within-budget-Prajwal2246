a1683c989a9e2ab216eea762f8749fd3239a4f45
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-02-11 16:14:43 +0530

    Refactor App component to manage budget input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [input, setInput] = useState("");
+
+  const items = [
+    { name: "Apple", price: 20 },
+    { name: "Banana", price: 10 },
+    { name: "Neutella", price: 30 },
+    { name: "Razor", price: 5 },
+    { name: "CornFlakes", price: 15 },
+    { name: "SoundBar", price: 50 },
+    { name: "Iphone", price: 80 },
+  ];
+
+  const budget = Number(input);
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <input
+        type="number"
+        placeholder="enter value"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
-export default App
+      <p>Items you can buy are in green color</p>
+      <table>
+        <tbody>
+          {items.map((item, idx) => (
+            <tr key={idx}>
+              <td>{item.name}</td>
+              <td
+                style={{
+                  color: budget && item.price <= budget ? "green" : "red",
+                }}
+              >
+                {item.price}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
